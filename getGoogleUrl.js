a19dc9f629ece2246bf7cbac0f06510e5bd19788
achen200 <54335096+achen200@users.noreply.github.com>
2024-04-04 18:43:05 -0700

    Server setup attempts

---
+++ getGoogleUrl.js
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+export function getGoogleOAuthURL(){
+	const rootUrl = 'https://accounts.google.com/o/oauth2/v2/auth'
+	const options = {
+		redirect_uri: process.env.NEXT_PUBLIC_GOOGLE_OAUTH_REDIRECT_URL, // as string,
+		client_id: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID, // as string,
+		access_type: 'offline',
+		response_type: 'code',
+		prompt: 'consent',
+		scope: ['https://www.googleapis.com/auth/documents']//.join(" ")
+	}
+
+	console.log(options);
+	const qs = new URLSearchParams(options);
+	console.log(qs);
+	return `${rootUrl}?${qs.toString()}`;
+}
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import { google } from "googleapis";
-
-const CLIENT_ID = '350552274341-f9up5g3hvo3646ll75ae5a2rh7f7q6jc.apps.googleusercontent.com';
-const CLIENT_SECRET = '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';
-
+import { getGoogleOAuthURL } from './getGoogleUrl.js';
 const app = express();
 const port = 3080;
+
+console.log(getGoogleOAuthURL());
+
 
 
 // const auth = new google.auth.GoogleAuth({
@@ -13,23 +13,23 @@ const port = 3080;
 // 	scopes: ['https://www.googleapis.com/auth/documents']
 // });
 
-const oauth2Client = new google.auth.OAuth2(
-	CLIENT_ID,
-	CLIENT_SECRET,
-	"/"
-  );
+// const oauth2Client = new google.auth.OAuth2(
+// 	CLIENT_ID,
+// 	CLIENT_SECRET,
+// 	"/"
+//   );
 
 
-app.listen(port, ()=>{
-    console.log(`Example app listening at port ${port}`);
-})
+// app.listen(port, ()=>{
+//     console.log(`Example app listening at port ${port}`);
+// })
 
-const url = oauth2Client.generateAuthUrl({
-	access_typeL:'offline',
-	scopes: ['https://www.googleapis.com/auth/documents']
-})
-const {tokens} = await oauth2Client.getToken()
-oauth2Client.setCredentials({access_token:tokens});
+// const url = oauth2Client.generateAuthUrl({
+// 	access_typeL:'offline',
+// 	scopes: ['https://www.googleapis.com/auth/documents']
+// })
+// const {tokens} = await oauth2Client.getToken()
+// oauth2Client.setCredentials({access_token:tokens});
 
 // const client = await google.docs({
 // 	version:"v1",
