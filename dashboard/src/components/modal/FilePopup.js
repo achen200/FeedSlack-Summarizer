aea82e50d94705cc9b556ce6973bc178bf046523
achen200 <54335096+achen200@users.noreply.github.com>
2024-04-07 20:03:39 -0700

    Support for uploading filetypes docx, pdf, xlsx, txt, log

--- dashboard/src/components/modal/FilePopup.js
+++ dashboard/src/components/modal/FilePopup.js
@@ -1,44 +1,91 @@
 import { useState } from "react";
-import axios from "axios";
 import "./Popups.css";
+import pdfToText from 'react-pdftotext';
+import { extractRawText } from 'mammoth';
+import * as XLSX from 'xlsx/xlsx.mjs';
 
+const reader = new FileReader();
 
 export const FilePopup = (props) => {
-	//Send props of adding new file
 	const [hide, setHide] = useState(true);
+	function togglePopup(){
+		setHide(!hide);
+	}
 
-	function showPopup(){
-		setHide(false);
-	}
-	function closePopup(){
-		setHide(true);
-	}
 	function handleSubmit(e){
 		e.preventDefault();
-		const filename = e.target.elements['fname'].value;
-		const originaltext = e.target.elements['original'].value;
-		
-		//Link handling in here...
-		//TODO: Change original text to link of google docs file, then send 
-		//HTTP request to retrieve docs data, THEN used retrieved data to 
-		//call openAI API
-
-		props.addNew(filename, originaltext);
-		closePopup();
+		const file = e.target[0].files[0];
+		const filename = file.name;
+		parseFile(filename, file, props.addNew);
+		togglePopup();
 	}
 
 	return !hide ? (
 		<div className="new-file-form">
-			<button onClick={closePopup}> x</button>
 			<form onSubmit={handleSubmit}>
-				<label htmlFor="fname">FileName</label>
-				<input type="text" id="fname" minLength="3" maxLength="20" required/>
-				<label htmlFor="original">Original Text</label>
-				<input type="text" id="original"/>
-				<input type="submit" value="Create"/>
+				<input type="file" name="file" required/>
+				<button>Upload</button>
 			</form>
+			<button onClick={togglePopup}>Cancel</button>
 		</div>
 	) : (
-		<div className="newfile-btn" onClick={showPopup}>New File</div>
+		<div className="newfile-btn" onClick={togglePopup}>New File</div>
 	);
+}
+
+/** Helper Functions */
+function getExtension(filename){
+	return filename.split('.').pop();
+}
+
+async function parseFile(filename, file, addfile){
+	const ext = getExtension(filename);
+
+	switch(ext){
+		case 'log':
+		case 'txt':
+			reader.readAsText(file);
+			reader.onloadend = () => addfile(filename, reader.result);
+			break;
+		case 'pdf': 	
+		case 'docx':	
+		case 'xlsx':
+			let text = await extractText(file, ext);
+			addfile(filename, text);
+			break;
+		default: //Attempt to read as text
+			alert("File type is not supported");
+	}
+}
+
+function extractText(file, type){
+	return new Promise((resolve, reject)=>{
+		try{
+			if(type==="pdf"){
+				pdfToText(file).then(text => resolve(text));
+				return;
+			}
+
+			reader.readAsArrayBuffer(file);
+			reader.onloadend = () => {
+				const data = reader.result;
+				if(type === "docx"){
+					extractRawText({arrayBuffer:data})
+						.then(text => resolve(text.value))
+						.catch(error => console.log(error));
+				}
+				else if (type === "xlsx"){
+					const workbook = XLSX.read(data);
+					let text = "";
+					workbook.SheetNames.forEach((name) => {
+						text += XLSX.utils.sheet_to_txt(workbook.Sheets[name]);
+					})
+					resolve(text);
+				}
+			}
+		}
+		catch(e){
+			reject(e);
+		}
+	})
 }
--- index.js
+++ index.js
@@ -4,36 +4,3 @@ import { getGoogleOAuthURL } from './getGoogleUrl.js';
 
 
 console.log(getGoogleOAuthURL());
-
-
-
-// const auth = new google.auth.GoogleAuth({
-// 	keyFilename: './authKey.json',
-// 	scopes: ['https://www.googleapis.com/auth/documents']
-// });
-
-// const oauth2Client = new google.auth.OAuth2(
-// 	CLIENT_ID,
-// 	CLIENT_SECRET,
-// 	"/"
-//   );
-
-
-// app.listen(port, ()=>{
-//     console.log(`Example app listening at port ${port}`);
-// })
-
-// const url = oauth2Client.generateAuthUrl({
-// 	access_typeL:'offline',
-// 	scopes: ['https://www.googleapis.com/auth/documents']
-// })
-// const {tokens} = await oauth2Client.getToken()
-// oauth2Client.setCredentials({access_token:tokens});
-
-// const client = await google.docs({
-// 	version:"v1",
-// 	auth:oauth2Client
-// })
-// const response = await client.documents.get({
-// 	documentId:"1DDT_4yroSxYeY_WNF9XkGnPIbkG0jFrIZ4pA-sfp02w"
-// });
